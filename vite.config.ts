b8bdd57e7c5bfb7e2e3e5b802c10f309538e0db4
Cell <celso.ferreira@hotmail.com>
2025-04-06 23:52:16 -0300

    Atualizando login e novas alterações

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LocDecor/', // 👈 ISSO É O QUE FALTAVA!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
